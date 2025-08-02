fac648bdab8bc1058a7fcbf001c1d2e88738fff9
Saroj Dahal <sarojdahal8848@gmail.com>
2025-08-02 20:08:05 +0900

    chore(database): setup initial

---
+++ src/main/java/com/saroj/simple_rest_api/entity/Note.java
@@ -0,0 +1,34 @@
+package com.saroj.simple_rest_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
