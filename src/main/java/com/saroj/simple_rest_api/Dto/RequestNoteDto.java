3ca57e25c8e27a0db8bb7f1fdc4d181950f82d27
Saroj Dahal <sarojdahal8848@gmail.com>
2025-08-02 23:42:43 +0900

    feat: add validation and exception

--- src/main/java/com/saroj/simple_rest_api/Dto/RequestNoteDto.java
+++ src/main/java/com/saroj/simple_rest_api/Dto/RequestNoteDto.java
@@ -1,9 +1,13 @@
 package com.saroj.simple_rest_api.Dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RequestNoteDto {
+    @NotNull(message = "Title must not be empty")
     private String title;
+    @NotNull(message = "Content must not be empty")
     private String content;
 }
