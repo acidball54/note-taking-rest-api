951956a042bb7d37a8d87ba4e2680318bece035b
Saroj Dahal <sarojdahal8848@gmail.com>
2025-08-02 20:16:09 +0900

    refactor(note): change column type to identity

--- src/main/java/com/saroj/simple_rest_api/entity/Note.java
+++ src/main/java/com/saroj/simple_rest_api/entity/Note.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class Note {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
